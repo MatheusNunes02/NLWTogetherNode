10dbe81941d81d4e3fe181e4153b54ca9a7289f7
matheusnunes123 <matheusnunesquarto@gmail.com>
2021-11-15 11:35:26 -0300

    NLW 2, Controllers, Database, Entities, Repositories, Services and Routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import "reflect-metadata";
 import express from 'express';
+import { router } from "./services/routes";
+
 
 import "./database"
 
 const app = express();
 
+//Definir que o express possa usar jsons
+app.use(express.json()); 
+
+//Utilizar as rotas do Router
+app.use(router);
+
+//Ouvir a porta 3000
 app.listen(3000, () => console.log("Server is running!!!"))
